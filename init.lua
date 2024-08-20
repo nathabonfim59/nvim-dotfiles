e77df4111ab9c9813550fb4b76c4ba490367fc1f
nathabonfim59 <dev@nathabonfim59.com>
2024-08-19 23:02:48 -0300

    fix(autoformatter): disable autoformatter in more filetypes

--- init.lua
+++ init.lua
@@ -715,7 +715,18 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = {
+					c = true,
+					cpp = true,
+					vue = true,
+					php = true,
+					css = true,
+					scss = true,
+					ctp = true,
+					javascript = true,
+					js = true,
+					conf = true,
+				}
 				return {
 					timeout_ms = 500,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
