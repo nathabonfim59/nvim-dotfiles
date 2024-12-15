ed76354d3d58f91ae23abf7b6069d16e697a709b
nathabonfim59 <dev@nathabonfim59.com>
2024-12-15 08:54:19 -0300

    feat(plugins): add avante.nvim

--- init.lua
+++ init.lua
@@ -205,6 +205,59 @@ require("lazy").setup({
 	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
 
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			provider = "copilot",
+			auto_suggestions_provider = "copilot",
+			auto_suggestions = true,
+			copilot = {
+				model = "claude-3.5-sonnet",
+				-- max_tokens = 4096,
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = false,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
+
 	{ "wakatime/vim-wakatime" },
 
 	-- Here is a more advanced example where we pass configuration
@@ -273,7 +326,6 @@ require("lazy").setup({
 			-- Only one of these is needed, not both.
 			"nvim-telescope/telescope.nvim", -- optional
 		},
-		config = true,
 	},
 
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
