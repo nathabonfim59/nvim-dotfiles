17a8b7c4cedebc20dfb18a17fdf9cd56baf79ad9
nathabonfim59 <dev@nathabonfim59.com>
2025-11-06 05:07:11 -0300

    feat(plugins): remove sidequick

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -8,10 +8,10 @@ return {
 		opts = {
 			provider = "copilot",
 			auto_suggestions_provider = "copilot",
-			auto_suggestions = false,
+			auto_suggestions = true,
 			providers = {
 				copilot = {
-					model = "gpt-4o",
+					model = "gpt-5-mini",
 				},
 				["kimi-k2"] = {
 					__inherited_from = "openai",
@@ -172,72 +172,6 @@ return {
 		},
 	},
 
-	{
-		"folke/sidekick.nvim",
-		opts = {
-			-- add any options here
-			cli = {
-				mux = {
-					backend = "zellij",
-					enabled = true,
-				},
-			},
-		},
-		keys = {
-			{
-				"<tab>",
-				function()
-					-- if there is a next edit, jump to it, otherwise apply it if any
-					if not require("sidekick").nes_jump_or_apply() then
-						return "<Tab>" -- fallback to normal tab
-					end
-				end,
-				expr = true,
-				desc = "Goto/Apply Next Edit Suggestion",
-			},
-			{
-				"<c-.>",
-				function()
-					require("sidekick.cli").focus()
-				end,
-				mode = { "n", "x", "i", "t" },
-				desc = "Sidekick Switch Focus",
-			},
-			{
-				"<leader>aa",
-				function()
-					require("sidekick.cli").toggle({ focus = true })
-				end,
-				desc = "Sidekick Toggle CLI",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>ac",
-				function()
-					require("sidekick.cli").toggle({ name = "claude", focus = true })
-				end,
-				desc = "Sidekick Claude Toggle",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>ag",
-				function()
-					require("sidekick.cli").toggle({ name = "grok", focus = true })
-				end,
-				desc = "Sidekick Grok Toggle",
-				mode = { "n", "v" },
-			},
-			{
-				"<leader>ap",
-				function()
-					require("sidekick.cli").select_prompt()
-				end,
-				desc = "Sidekick Ask Prompt",
-				mode = { "n", "v" },
-			},
-		},
-	},
-
 	-- Github Copilot
 	{
 		"zbirenbaum/copilot.lua",
@@ -246,7 +180,7 @@ return {
 		config = function()
 			require("copilot").setup({
 				suggestion = {
-					enabled = false,
+					enabled = true,
 					auto_trigger = true, -- Enable auto-trigger
 					debounce = 75,
 					keymap = {
