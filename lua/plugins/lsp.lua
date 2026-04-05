7dc777437adb20d736859a814dc46a5d798f41a1
nathabonfim59 <dev@nathabonfim59.com>
2026-04-05 14:00:27 -0300

    feat: update for compatibility with neovim 0.12

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
 
 			-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
 			-- used for completion, annotations and signatures of Neovim apis
-			{ "folke/neodev.nvim", opts = {} },
+			{ "folke/lazydev.nvim", ft = "lua", opts = {} },
 
 			-- Database browser
 			{
@@ -171,70 +171,41 @@ return {
 				end,
 			})
 
-			-- LSP servers and clients are able to communicate to each other what features they support.
-			--  By default, Neovim doesn't support everything that is in the LSP specification.
-			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities())
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+				handlers = {
+					function() end,
+				},
+			})
+			require("mason-tool-installer").setup({
+				ensure_installed = { "stylua" },
+			})
 
-			-- Enable the following language servers
-			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-			--
-			--  Add any additional override configuration in the following tables. Available keys are:
-			--  - cmd (table): Override the default command used to start the server
-			--  - filetypes (table): Override the default list of associated filetypes for the server
-			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-			--  - settings (table): Override the default settings passed when initializing the server.
-			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-			local servers = {
-				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-							-- diagnostics = { disable = { 'missing-fields' } },
-						},
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			vim.lsp.config["lua_ls"] = {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						completion = { callSnippet = "Replace" },
 					},
 				},
 			}
 
-			-- Ensure the servers and tools above are installed
-			--  To check the current status of installed tools and/or manually install
-			--  other tools, you can run
-			--    :Mason
-			--
-			--  You can press `g?` for help in this menu.
-			require("mason").setup()
+			vim.lsp.enable({ "lua_ls" })
 
-			-- You can add other tools here that you want Mason to install
-			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {})
-			vim.list_extend(ensure_installed, {
-				"stylua", -- Used to format Lua code
-			})
-			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
-			require("mason-lspconfig").setup({
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for tsserver)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
-
-						local capabilities = require("blink.cmp").get_lsp_capabilities()
-						local lspconfig = require("lspconfig")
-
-						lspconfig[server_name].setup({ capabilities = capabilities })
-					end,
+			local sev = vim.diagnostic.severity
+			vim.diagnostic.config({
+				severity_sort = true,
+				float = { border = "rounded", source = true },
+				signs = {
+					text = {
+						[sev.ERROR] = "E",
+						[sev.WARN] = "W",
+						[sev.INFO] = "I",
+						[sev.HINT] = "H",
+					},
 				},
 			})
 		end,
