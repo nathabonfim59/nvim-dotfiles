1ea4cb354579f541556ee1373b4181bbba79c2f1
nathabonfim59 <dev@nathabonfim59.com>
2026-05-20 12:33:35 -0300

    fix: treesitter install warning

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,23 @@ return {
 	-- Treesitter parser and query installer
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
 		main = "nvim-treesitter",
+		init = function()
+			if vim.fn.executable("tree-sitter") ~= 1 then
+				vim.notify(
+					[[tree-sitter CLI not found. Parsers will fail to compile.
+
+Install it with one of:
+  mise use -g tree-sitter@latest
+  npm install -g tree-sitter-cli
+  bun install -g tree-sitter-cli]],
+					vim.log.levels.WARN,
+					{ title = "nvim-treesitter" }
+				)
+			end
+		end,
 		opts = {
 			ensure_installed = {
 				"bash",
